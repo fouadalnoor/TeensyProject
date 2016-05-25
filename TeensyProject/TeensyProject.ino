28d13405195ce9e545b87095f19b8b2b90c59947
=Fouad Al-noor <fouadalnoor@hotmail.com>
2016-05-25 17:27:21 +0100

    Created two interrupts. Both of them can work at the same time. It appears that I only need to use one interrupt to wake up the chip, sample ADC, check if its >2V and then go back to sleep (if it's less than 2V). If its not less than 2V, then go to the loop.

--- TeensyProject/TeensyProject.ino
+++ TeensyProject/TeensyProject.ino
@@ -81,7 +81,7 @@
 #include "Test.h"
 //#include "SoftSPI.h"
 #include "LowPower_Teensy3_mod.h"
-//#include "TimerOne.h"
+#include "TimerOne.h"
 //#include "DS3231RTC.h"
 
 
@@ -96,20 +96,49 @@
 int analogue_value;
 int adc_resolution;
 bool en = true;
+float final_analogue_voltage;
 
 TEENSY3_LP LP = TEENSY3_LP(); //Create Object for sleep
 
 
 void callbackhandler() {
+    
+    Serial.print("Entered Callback Handler"); //only accurate to about 2 decimal places. Supply limit might play a role.
+    Serial.print("\n");
+    
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
+    delay(1000);
     digitalWrite(LED_BUILTIN, LOW);
     
     loop();//Go back to the loop when awake again.
 }
 
-void exampleInterruptFunction() {
- Serial.print("Interrupt Executed\n");
+void sleepInterrupt() {
+ 
+ Serial.print("Sleep Interrupt Entered\n");
+ Serial.print("\n");
+    
+ //Measure voltage
+    
+
+    
+    Serial.print("Voltage value is: ");
+    Serial.print(final_analogue_voltage); //only accurate to about 2 decimal places. Supply limit might play a role.
+    Serial.print("\n");
+    
+  //check battery voltage is below 2V (should be 3.4V), then sleep
+    
+    /*
+    if(final_analogue_voltage<2)
+    {
+        Serial.print("Enter Sleep mode"); //only accurate to about 2 decimal places. Supply limit might play a role.
+        Serial.print("\n");
+        
+        LP.Sleep(); //May disable Timer1?
+    }
+     
+     */
+ 
 }
 
 // Add setup code
@@ -122,12 +151,13 @@ void setup()
     
     //Set  pin 0 as an input and attach it as an interrupt (for sleeping purposes).
     pinMode(0, INPUT);
-    attachInterrupt(0, callbackhandler, CHANGE); //Only change from Low-High works?
+    attachInterrupt(0, callbackhandler, CHANGE); //Only change from Low-High works? - Seems like both work. 
     
     //Example Timer as an interrupt trigger - Need the .h file.
     
-    //Timer1.initialize(150000);
-    //Timer1.attachInterrupt(exampleInterruptFunction);
+    Timer1.initialize();
+    Timer1.attachInterrupt(sleepInterrupt, 1000000); //Max time period is 1ms?
+    
 
    
     
@@ -152,18 +182,19 @@ void loop()
     }
      */
     
-    digitalWrite(13, HIGH);
-    delay(100);
-    digitalWrite(13, LOW);
-    delay(100);
-    
-    
     int analogue_value = adc->analogRead(A9);
     float analogue_voltage = (float)analogue_value; //19275;//convert to voltage
     float dividend = 19275.0;
-    float final_analogue_voltage = analogue_voltage/dividend;
-    String finalStringValue = (String)final_analogue_voltage;
-
+     final_analogue_voltage = analogue_voltage/dividend;
+    
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+    
+    
+    
+    
     
     //Serial.print("Analogue Value: \n");
     //Serial.print(analogue_voltage);
@@ -172,13 +203,13 @@ void loop()
    // Serial.print(analogue_value, 3);
    // Serial.print("\n");
     
-    Serial.print(" ");
-    Serial.print(finalStringValue); //only accurate to about 2 decimal places. Supply limit might play a role.
-    delay(1000);
+   
     
     
   
     //The value should be <3.4
+    
+    
     if(final_analogue_voltage<2)
     {
         //sleep
@@ -193,6 +224,8 @@ void loop()
        
         LP.Sleep();
     }
+     
+    
    
     
 }
